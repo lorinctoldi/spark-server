2c7a3e941cd4a874c92d14b59ec828661e9be260
lorinc <lorinc.toldi@gmail.com>
2023-12-14 03:01:20 +0100

    save

--- server.js
+++ server.js
@@ -1,63 +1,56 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
 const sharp = require('sharp');
 const fs = require('fs');
+const path = require('path');
+const cors = require('cors')
+
 
 const app = express();
 const port = 3001;
 
-// Create the 'uploads' directory if it doesn't exist
+app.use(cors());
+
 const uploadsDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir);
 }
 
-// Set up multer for handling file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
+const storage = multer.memoryStorage(); // Use memory storage to get the file buffer in memory
 const upload = multer({ storage: storage });
 
-// Serve the frontend
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-// Handle file uploads
 app.post('/upload', upload.array('images'), async (req, res) => {
   try {
-    // Assuming you want to process each uploaded image individually
-    for (const file of req.files) {
-      const inputPath = path.join(__dirname, 'uploads', file.filename);
-      const outputPath = path.join(__dirname, 'uploads/processed', file.filename);
+    const processedImages = [];
 
-      await cropImage(inputPath, outputPath, 1200, 1200);
+    for (const file of req.files) {
+      const processedImageBuffer = await cropImage(file.buffer, file.originalname, 1200, 1200);
+      const base64Image = processedImageBuffer.toString('base64');
+      processedImages.push({ name: file.originalname, data: base64Image });
     }
 
     console.log('Files uploaded and processed successfully');
-    res.status(200).send('Files uploaded and processed successfully');
+    res.status(200).json({ success: true, processedImages });
   } catch (error) {
     console.error('Error uploading and processing images:', error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-async function getAllPixels(filePath) {
+async function getAllPixels(buffer) {
   try {
-    const image = sharp(filePath);
+    const image = sharp(buffer);
 
-    // Explicitly specify the number of channels (RGB images have 3 channels)
-    const pixelData = await image.raw().toBuffer({ resolveWithObject: true, channels: 3 });
+    // Ensure we set the number of channels appropriately based on the input image
+    const { channels } = await image.metadata();
+
+    const pixelData = await image.raw().toBuffer({ resolveWithObject: true, channels });
     const { data, info } = pixelData;
 
     const width = info.width;
@@ -67,14 +60,15 @@ async function getAllPixels(filePath) {
     for (let y = 0; y < height; y++) {
       pixels.push([]);
       for (let x = 0; x < width; x++) {
-        const offset = (y * width + x) * 3;
+        const offset = (y * width + x) * channels;
         const r = data[offset];
         const g = data[offset + 1];
         const b = data[offset + 2];
-        pixels[y].push([ r, g, b ]);
+
+        pixels[y].push([r, g, b]);
       }
     }
-    
+
     return { height, width, pixels };
   } catch (error) {
     console.error('Error getting pixels:', error);
@@ -126,42 +120,72 @@ function getBorders(set, size) {
   return [start, end];
 }
 
-async function cropImage(inputPath, outputPath, targetHeight, targetWidth) {
-  const pixelData = await getAllPixels(inputPath);
-  const whiteRows = new Set();
-  const whiteCols = new Set();
+async function cropImage(buffer, filename, targetHeight, targetWidth) {
+  try {
+    console.log('__dirname:', __dirname);
+    console.log('filename:', filename);
 
-  const treshold = getAverage(pixelData.pixels.flat().flat()) * 1.03;
+    const pixelData = await getAllPixels(buffer);
+    const whiteRows = new Set();
+    const whiteCols = new Set();
 
-  console.log(pixelData.width, pixelData.height)
-  for(let x = 0; x < pixelData.width; x++)
-    if(isWhiteRow(x, pixelData.height, pixelData.pixels, treshold+4)) whiteCols.add(x);
-  for(let y = 0; y < pixelData.height; y++)
-    if(isWhiteColumn(y, pixelData.width, pixelData.pixels, treshold+4)) whiteRows.add(y);
+    const treshold = getAverage(pixelData.pixels.flat().flat()) * 1.03;
 
-  console.log(whiteCols, whiteRows)
-  const [colStart, colEnd] = getBorders(whiteCols, whiteCols.size)
-  const [rowStart, rowEnd] = getBorders(whiteRows, whiteRows.size)
+    for (let x = 0; x < pixelData.width; x++)
+      if (isWhiteRow(x, pixelData.height, pixelData.pixels, treshold + 4)) whiteCols.add(x);
+    for (let y = 0; y < pixelData.height; y++)
+      if (isWhiteColumn(y, pixelData.width, pixelData.pixels, treshold + 4)) whiteRows.add(y);
 
-  const resizedImage = await sharp(inputPath)
-    .extract({ left: colStart, top: rowStart, width: colEnd - colStart, height: rowEnd - rowStart })
-    .resize(targetWidth, targetHeight, { fit: 'contain', position: 'centre', background: 'white' })
-    .toBuffer();
+    const [colStart, colEnd] = getBorders(whiteCols, whiteCols.size);
+    const [rowStart, rowEnd] = getBorders(whiteRows, whiteRows.size);
 
-  await sharp({
-    create: {
-      width: 1620,
-      height: 1620,
-      channels: 3,
-      background: 'white',
-    },
-  })
-    .composite([
-      {
-        input: resizedImage,
-        left: Math.floor((1620 - targetWidth) / 2),
-        top: Math.floor((1620 - targetHeight) / 2),
-      },
-    ])
-    .toFile(outputPath);
+    console.log('Crop Coordinates:', colStart, colEnd, rowStart, rowEnd);
+
+    // Ensure that the crop coordinates are valid before using sharp
+    if (colStart !== undefined && colEnd !== undefined && rowStart !== undefined && rowEnd !== undefined) {
+      // Use sharp directly with the buffer and obtain the processed image as a buffer
+      const croppedImage = await sharp(buffer)
+        .extract({ left: colStart, top: rowStart, width: colEnd - colStart, height: rowEnd - rowStart })
+        .resize(targetWidth, targetHeight, { fit: 'contain', position: 'centre', background: 'white' })
+        .toBuffer();
+
+        const fileExtension = 'webp';
+
+        // Create the output path with the original filename and WebP extension
+        const outputPath = path.join(__dirname, 'uploads', `${path.parse(filename).name}.${fileExtension}`);
+
+        const resizedImage = await sharp({
+          create: {
+            width: 1620,
+            height: 1620,
+            channels: 3,
+            background: 'white',
+          },
+        })
+        .composite([
+          {
+            input: croppedImage,
+            left: Math.floor((1620 - targetWidth) / 2),
+            top: Math.floor((1620 - targetHeight) / 2),
+          },
+        ])
+        .toFormat("webp")
+        .toBuffer();
+
+        console.log(filename)
+
+        // Now you can save the processedImageBuffer or do further processing as needed
+        fs.writeFileSync(outputPath, resizedImage);
+
+
+      console.log('Image processed successfully');
+
+      return resizedImage;
+    } else {
+      console.error('Error: Invalid crop coordinates');
+    }
+  } catch (error) {
+    console.error('Error processing image:', error);
+  }
 }
+
